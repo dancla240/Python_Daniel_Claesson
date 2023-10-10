87b74cf121577faa3587db64b92f4f0e8f35fb15
Daniel Claesson <dacl@rocketmail.com>
2023-10-10 07:43:01 +0200

    Updates to Lab3. Added more unit tests. Moven str and repr methods into circle and rectangle classes, from parant class. Some refactoring.

--- Lab3/Lab3_functions.py
+++ Lab3/Lab3_functions.py
@@ -9,18 +9,6 @@ class GeometricFigure:
 		self.x = self.x + delta_x
 		self.y = self.y + delta_y
 	
-	def __str__(self):
-		if hasattr(self, 'is_quadratic'):
-			return f'__str__ :\nx = {self.x}\ny = {self.y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}'
-		elif hasattr(self, 'is_unitcircle'):
-			return f'_str_ :\nx = {self.x}\ny = {self.y}\nradius = {self.radius}\narea = {self.area}'
-	
-	def __repr__(self):
-		if hasattr(self, 'is_quadratic'):
-			return f'__repr__ :\nx = {self.x}\ny = {self.y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}'
-		elif hasattr(self, 'is_unitcircle'):
-			return f'__repr__ :\nx = {self.x}\ny = {self.y}\nradius = {self.radius}\narea = {self.area}'
-		
 	def __eq__(self, other):
 		return self.area == other.area
 			
@@ -49,13 +37,18 @@ class Circle(GeometricFigure):
 	@property	
 	def circumference(self):
 		return math.pi * 2 * self.radius
-		
-			
+					
 	def is_inside(self, other_x, other_y):
 		return math.sqrt((self.x - other_x)**2 + (self.y - other_y)**2) < self.radius
 	
 	def is_unitcircle(self):
 		return self.radius == 1 and self.x == 0 and self.y == 0
+	
+	def __str__(self):
+		return f'_str_ :\nx = {self.x}\ny = {self.y}\nradius = {self.radius}\narea = {self.area}\ncircumference = {self.circumference}\nis unitcircle = {self.is_unitcircle()}'
+	
+	def __repr__(self):
+		return f'_repr_ :\nx = {self.x}\ny = {self.y}\nradius = {self.radius}\narea = {self.area}\ncircumference = {self.circumference}\nis unitcircle = {self.is_unitcircle()}'
 		
 class Rectangle(GeometricFigure):
 	def __init__(self, x, y, side1, side2):
@@ -77,4 +70,10 @@ class Rectangle(GeometricFigure):
 		
 	def is_inside(self, point):
 		return f'to be done'
+
+	def __str__(self):
+		#return f'__str__ :\nx = {self.x}\ny = {self.y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}\ncircumference = {self.circumference}\nis quadratic = {self.is_quadratic()}'
+		return f'__str__ :\nx = {self.x}\ny = {self.y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}'
+	def __repr__(self):
+		return f'__repr__ :\nx = {self.x}\ny = {self.y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}\ncircumference = {self.circumference}\nis quadratic = {self.is_quadratic}'
 		
--- Lab3/Lab3_unit_test.py
+++ Lab3/Lab3_unit_test.py
@@ -1,6 +1,26 @@
 import Lab3_functions as fkn
 import math
 
+def test_rectangle():
+    rectangle1 = fkn.Rectangle(1,1,1,1)
+    assert rectangle1.area == 1
+    assert rectangle1.circumference == 4
+    assert rectangle1.x == 1
+    assert rectangle1.y == 1
+    assert rectangle1.is_quadratic() == True
+    rectangle2 = fkn.Rectangle(1,1,1,1)
+    assert rectangle2 == rectangle1
+    assert rectangle2 >= rectangle1
+    rectangle3 = fkn.Rectangle(2,2,1,2)
+    assert rectangle3 != rectangle1
+    assert rectangle3.is_quadratic() == False
+    rectangle3.translation(1,1)
+    assert rectangle3.x == 3
+    assert rectangle3.y == 3
+    assert rectangle3 > rectangle1
+    assert rectangle1 < rectangle3
+
+
 def test_circle():
     circle1 = fkn.Circle(0,0,1)
     assert circle1.area == 1*1*math.pi
@@ -9,11 +29,18 @@ def test_circle():
     assert circle1.is_inside(1,0) == False
     assert circle1.is_inside(0.5,0.5) == True
     assert circle1.is_inside(0.9999,0) == True
+    assert circle1.is_unitcircle() == True
+    circle2 = fkn.Circle(0,0,1)
+    assert circle1 == circle2
+    circle1.translation(2,2)
+    assert circle1.is_inside(0,0) == False
+    assert circle1.is_inside(2,2) == True
+    assert circle1.is_unitcircle() == False
+    assert circle1 == circle2
+    assert circle1 >= circle2
 
-def test_rectangle():
+def test_circle_rectangle():
+    circle1 = fkn.Circle(0,0,1)
     rectangle1 = fkn.Rectangle(1,1,1,1)
-    assert rectangle1.area == 1
-    assert rectangle1.x == 1
-    assert rectangle1.y == 1
-    assert rectangle1.circumference == 4
-    assert rectangle1.is_quadratic == True
+    assert circle1.area > rectangle1.area
+
