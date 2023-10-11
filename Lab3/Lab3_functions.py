2dee063baf79138cc67a5e8452086d26a01d3e90
Daniel Claesson <dacl@rocketmail.com>
2023-10-11 20:36:49 +0200

    Functions updated. Coordinate properties made private + other refactoring

--- Lab3/Lab3_functions.py
+++ Lab3/Lab3_functions.py
@@ -1,7 +1,5 @@
 import math
 
-"""man ska göra tester på te xom man skriver in en sträng som """
-
 class GeometricFigure:
 	"""Parent class for creating geometric figures.
 	Contains attributes and methods common between
@@ -14,13 +12,29 @@ class GeometricFigure:
 		
 		self.x = x
 		self.y = y
+
+	@property
+	def x(self):
+		return self._x
+
+	@x.setter
+	def x(self, x):
+		self._x = x
+
+	@property
+	def y(self):
+		return self._y
+	
+	@y.setter
+	def y(self, y):
+		self._y = y
 		
 	def translation(self,delta_x, delta_y):
 		if not all(isinstance(value, Circle.allowed_types) for value in [delta_x, delta_y]):
 			raise TypeError('Allowed translation inputs are integers or floats.')
 		
-		self.x = self.x + delta_x
-		self.y = self.y + delta_y
+		self._x = self._x + delta_x
+		self._y = self._y + delta_y
 	
 	def __eq__(self, other):
 		return self.area == other.area
@@ -60,16 +74,16 @@ class Circle(GeometricFigure):
 		return math.pi * 2 * self.radius
 					
 	def is_inside(self, other_x, other_y):
-		return math.sqrt((self.x - other_x)**2 + (self.y - other_y)**2) < self.radius
+		return math.sqrt((self._x - other_x)**2 + (self._y - other_y)**2) < self.radius
 	
 	def is_unitcircle(self):
-		return self.radius == 1 and self.x == 0 and self.y == 0
+		return self.radius == 1 and self._x == 0 and self._y == 0
 	
 	def __str__(self):
-		return f'_str_ :\nx = {self.x}\ny = {self.y}\nradius = {self.radius}\narea = {self.area}\ncircumference = {self.circumference}\nis unitcircle = {self.is_unitcircle()}'
+		return f'_str_ :\nx = {self._x}\ny = {self._y}\nradius = {self.radius}\narea = {self.area}\ncircumference = {self.circumference}\nis unitcircle = {self.is_unitcircle()}'
 	
 	def __repr__(self):
-		return f'_repr_ :\nx = {self.x}\ny = {self.y}\nradius = {self.radius}\narea = {self.area}\ncircumference = {self.circumference}\nis unitcircle = {self.is_unitcircle()}'
+		return f'_repr_ :\nx = {self._x}\ny = {self._y}\nradius = {self.radius}\narea = {self.area}\ncircumference = {self.circumference}\nis unitcircle = {self.is_unitcircle()}'
 		
 class Rectangle(GeometricFigure):
 	"""Sub class to class Geometric figures, containing properties,
@@ -96,12 +110,12 @@ class Rectangle(GeometricFigure):
 	def is_quadratic(self):
 		return self.side1 == self.side2
 		
-	def is_inside(self, point):
-		return f'to be done'
+	def is_inside(self, other_x, other_y):
+		return other_x < (self._x + self.side1/2) and other_x > (self._x - self.side1/2) and other_y < (self._y + self.side2/2) and other_y > (self._y - self.side2/2)
 
 	def __str__(self):
-		return f'__str__ :\nx = {self.x}\ny = {self.y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}\ncircumference = {self.circumference}\nis quadratic = {self.is_quadratic()}'
+		return f'__str__ :\nx = {self._x}\ny = {self._y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}\ncircumference = {self.circumference}\nis quadratic = {self.is_quadratic()}'
 		
 	def __repr__(self):
-		return f'__repr__ :\nx = {self.x}\ny = {self.y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}\ncircumference = {self.circumference}\nis quadratic = {self.is_quadratic()}'
+		return f'__repr__ :\nx = {self._x}\ny = {self._y}\nside1 = {self.side1}\nside2 = {self.side2}\narea = {self.area}\ncircumference = {self.circumference}\nis quadratic = {self.is_quadratic()}'
 		
