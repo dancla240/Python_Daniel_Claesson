3f0cac9aabda9af45027f5ad417201ba8b4b8087
Daniel Claesson <dacl@rocketmail.com>
2023-10-19 11:28:11 +0200

    Refactoring.

--- Lab3/plot_cube.py
+++ Lab3/plot_cube.py
@@ -1,16 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import axes3d
-from cube import Cube
 
-# https://www.youtube.com/watch?v=ics0b1qupIc
+
+# Used this video as inspiration: https://www.youtube.com/watch?v=ics0b1qupIc
 
 def plot_cube(side1, side2, side3):
-    cube = np.ones((side1, side2, side3), dtype = bool)
+    cube = np.ones((side1, side2, side3), dtype=bool)
     fig = plt.figure()
-    ax = plt.axes(projection = '3d')
-    ax.set_facecolor("Cyan")
-    ax.voxels(cube, facecolor = "#E02050", edgecolors="k")
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.set_xlim(0, 10)
+    ax.set_ylim(0, 10)
+    ax.set_zlim(0, 10)
+
+    ax.set_xlabel('X Axis')
+    ax.set_ylabel('Y Axis')
+    ax.set_zlabel('Z Axis')
+
+    ax.voxels(cube, facecolors="#E02050", edgecolors="k")
     ax.axis('on')
-    #plt.plot()
+
     plt.show()
